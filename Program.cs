7299aab796980f5393efcb82c021a0ab821e6caf
M.F.M Fazrin <mohamed.farzin@idsnext.com>
2021-06-18 03:38:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace null_coalescing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coalescing cl = new Coalescing();
+            cl.Nullable();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+    public class Coalescing
+    {
+        private int? x = null;
+        private int y = 9;
+        public Coalescing()
+        {
+
+        }
+        double SumNumbers(List<double[]> setsOfNumbers, int indexOfSetToSum)
+        {
+            return setsOfNumbers?[indexOfSetToSum]?.Sum() ?? double.NaN;
+        }
+        public void Nullable()
+        {
+            var sum = SumNumbers(null, 0);
+            Console.WriteLine(sum);  // output: NaN
+        }
+    }
+}
